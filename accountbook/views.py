09a5cc97c12b96c48da83a622b68e4ee34fd6a7c
ppo3330 <junseok3330@gmail.com>
2025-11-28 14:25:37 +0900

    feat:가계부 리스트 페이지 구현

--- accountbook/views.py
+++ accountbook/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Record
 
-# Create your views here.
+def record_list(request):
+    records = Record.objects.all().order_by('-date', '-id')
+
+    return render(request, 'accountbook/list.html', {
+        'records': records
+    })
