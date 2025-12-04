15d0b8124bcec1beab7a768827923b4e442608b0
ppo3330 <junseok3330@gmail.com>
2025-12-04 15:57:43 +0900

    feat: 수정 삭제 및 통계 보기 기능 추가

--- accountbook/urls.py
+++ accountbook/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.record_list, name='record_list'),
     path('create/', views.record_create, name='record_create'),
     path('<int:pk>/', views.record_detail, name='record_detail'),
+    path('<int:pk>/edit/', views.record_edit, name='record_edit'),
+    path('<int:pk>/delete/', views.record_delete, name='record_delete'),
+    path('stats/', views.stats, name='stats'),
 ]
--- accountbook/views.py
+++ accountbook/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Record
+from django.db.models import Sum
 
 def record_list(request):
     records = Record.objects.all().order_by('-date', '-id')
-
+    
     return render(request, 'accountbook/list.html', {
         'records': records
     })
@@ -31,4 +32,52 @@ def record_detail(request,pk):
     record = get_object_or_404(Record, pk=pk)
     return render(request, 'accountbook/detail.html',{
         'record':record
+    })
+
+def record_edit(request,pk):
+    record = get_object_or_404(Record, pk=pk)
+    if request.method == "POST":
+        title = request.POST.get('title')
+        amount = request.POST.get('amount')
+        record_type = request.POST.get('record_type')
+        category = request.POST.get('category')
+        date = request.POST.get('date')
+
+        if title and amount and record_type and category and date:
+            record.title = title
+            record.amount = amount
+            record.record_type = record_type
+            record.category = category
+            record.date = date
+            record.save()
+
+            return redirect('record_detail', pk=record.pk)
+
+    return render(request, 'accountbook/record_form.html',{
+        'record' : record
+    })
+
+def record_delete(request,pk):
+    record = get_object_or_404(Record, pk=pk)
+
+    record.delete()
+    return redirect('record_list')
+
+
+def stats(request):
+    income_stats = (
+        Record.objects.filter(record_type='income')
+        .values('category')
+        .annotate(total=Sum('amount'))
+    )
+
+    expense_stats = (
+        Record.objects.filter(record_type='expense')
+        .values('category')
+        .annotate(total=Sum('amount'))
+    )
+
+    return render(request, 'accountbook/stats.html', {
+        'income_stats': income_stats,
+        'expense_stats': expense_stats,
     })
